fd8bc1e8358245b87dd1cc911632596bbb45bc0f
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 08:12:11 +0200

    refactored

--- university.sql
+++ university.sql
@@ -2,32 +2,52 @@
 
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
-SELECT `date_of_birth` FROM `students` WHERE `date_of_birth` LIKE '1990-%';
+SELECT `date_of_birth` 
+FROM `students`
+WHERE `date_of_birth`
+LIKE '1990-%';
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
   --SELECT `cfu` FROM `courses` where `cfu` > 10;
 
-SELECT * FROM `courses` WHERE `cfu`> 10;
+SELECT * 
+FROM `courses` 
+WHERE `cfu`> 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
-SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992;
+SELECT * 
+FROM `students` 
+WHERE YEAR(`date_of_birth`) < 1992;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
-SELECT * FROM `courses` WHERE `year`= 1 AND `period`= 'I semestre';
+SELECT * 
+FROM `courses` 
+WHERE `year`= 1 
+AND `period`= 'I semestre';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour`> '14:00:00';
+SELECT * 
+FROM `exams` 
+WHERE `date` = '2020-06-20' 
+AND `hour`> '14:00:00';
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
-SELECT * FROM `degrees` WHERE `name` LIKE '%magistrale%';
+SELECT * 
+FROM `degrees` 
+WHERE `name` 
+LIKE '%magistrale%';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
-SELECT COUNT(*) FROM `departments`;
+SELECT COUNT(*) 
+FROM `departments`;
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
-SELECT COUNT(*) FROM `teachers` WHERE `phone`IS NULL;
+SELECT COUNT(*) 
+FROM `teachers` 
+WHERE `phone`
+IS NULL;
 
 -- ------------------------------------------ QUERY CON GROUP BY
 
