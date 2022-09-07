486e433a2a50282f1ae879d2afa2344e34e76e08
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 08:14:33 +0200

    group by -1st

--- university.sql
+++ university.sql
@@ -53,7 +53,9 @@ IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS `n_studenti`, YEAR(`enrolment_date`) AS `anno_iscrizione`
+FROM `students`
+GROUP BY `anno_iscrizione` ;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
