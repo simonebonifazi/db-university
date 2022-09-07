1cd2a0d316aa86d80dac6da856aa022fe1f73c39
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 07:36:29 +0200

    added first ex

--- university.sql
+++ university.sql
@@ -5,7 +5,7 @@
 SELECT `date_of_birth` FROM `students` WHERE `date_of_birth` LIKE '1990-%';
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-
+  
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
