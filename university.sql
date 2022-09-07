30d9d3b93b7d6b5056d92683c392d7a4dfc8f232
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 08:08:46 +0200

    6added

--- university.sql
+++ university.sql
@@ -18,10 +18,10 @@ SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992;
 SELECT * FROM `courses` WHERE `year`= 1 AND `period`= 'I semestre';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour`> '14:00:00'
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour`> '14:00:00';
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
-
+SELECT * FROM `degrees` WHERE `name` LIKE '%magistrale%';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
 
