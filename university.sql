939279b79038e0e103ac2eb852dcbf7416dbecd8
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 07:47:02 +0200

    second added

--- university.sql
+++ university.sql
@@ -5,7 +5,9 @@
 SELECT `date_of_birth` FROM `students` WHERE `date_of_birth` LIKE '1990-%';
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-  
+  SELECT `cfu` FROM `courses` where `cfu` > 10;
+
+  --SELECT * FROM `courses` WHERE `cfu`> 9;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
