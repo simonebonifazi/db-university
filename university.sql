b7ab30ad5a76767c7669b991bc5f63fee7dd4cf3
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 08:07:03 +0200

    5add

--- university.sql
+++ university.sql
@@ -18,7 +18,7 @@ SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992;
 SELECT * FROM `courses` WHERE `year`= 1 AND `period`= 'I semestre';
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
-
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour`> '14:00:00'
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
 
