63fa42d22cfb51384410eb0668fb7cbcf824f6dd
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 08:11:11 +0200

    7 and 8 added to refact

--- university.sql
+++ university.sql
@@ -24,10 +24,10 @@ SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour`> '14:00:00';
 SELECT * FROM `degrees` WHERE `name` LIKE '%magistrale%';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
-
+SELECT COUNT(*) FROM `departments`;
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
-
+SELECT COUNT(*) FROM `teachers` WHERE `phone`IS NULL;
 
 -- ------------------------------------------ QUERY CON GROUP BY
 
