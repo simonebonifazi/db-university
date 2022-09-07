35d4c8924ad316ae56fc628e3005aca76019f997
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 07:55:26 +0200

    2 fixed and trihrd added

--- university.sql
+++ university.sql
@@ -5,12 +5,13 @@
 SELECT `date_of_birth` FROM `students` WHERE `date_of_birth` LIKE '1990-%';
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-  SELECT `cfu` FROM `courses` where `cfu` > 10;
+  --SELECT `cfu` FROM `courses` where `cfu` > 10;
 
-  --SELECT * FROM `courses` WHERE `cfu`> 9;
+SELECT * FROM `courses` WHERE `cfu`> 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
+SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
