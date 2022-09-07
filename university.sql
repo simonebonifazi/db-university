8aadb7e8dd09c398c62d25cc0311ebfb8e3ce6be
simonebonifazi <simonbonifraz@gmail.com>
2022-09-07 08:25:28 +0200

    3add

--- university.sql
+++ university.sql
@@ -63,6 +63,9 @@ FROM `teachers`
 GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT AVG(`vote`) 
+FROM `exam_student` 
+GROUP BY `exam_id` 
+--ORDER BY AVG(`vote`) DESC;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
