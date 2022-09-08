89d014e016c595c8d523c99e7d699a24fcab2341
simonebonifazi <simonbonifraz@gmail.com>
2022-09-08 07:09:11 +0200

    1_added

--- sql/uni_with_join.sql
+++ sql/uni_with_join.sql
@@ -1,5 +1,13 @@
 -- ------------------------------------------ QUERY CON JOIN
+
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id` =  `students`.`degree_id`
+
+
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
