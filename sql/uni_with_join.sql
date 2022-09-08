bb94e43b776bc9789a0c8bf48108f1cb3ccd904f
simonebonifazi <simonbonifraz@gmail.com>
2022-09-08 07:14:10 +0200

    added _ 2

--- sql/uni_with_join.sql
+++ sql/uni_with_join.sql
@@ -10,7 +10,13 @@ ON `degrees`.`id` =  `students`.`degree_id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+ 
+ SELECT * 
+FROM `degrees`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
 
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
