b7a1ec55e4ebfc073f846a0b678413385252c712
simonebonifazi <simonbonifraz@gmail.com>
2022-09-08 07:20:07 +0200

    3_added

--- sql/uni_with_join.sql
+++ sql/uni_with_join.sql
@@ -19,7 +19,11 @@ ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-
+SELECT * 
+FROM `course_teacher` 
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `teachers`.`id` = 44;
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
 
