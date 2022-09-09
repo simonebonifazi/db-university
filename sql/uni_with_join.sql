88ce6a56f7ab0c9e9e0e147fff6bee97b46bc1a6
simonebonifazi <simonbonifraz@gmail.com>
2022-09-09 05:44:40 +0200

    5_added

--- sql/uni_with_join.sql
+++ sql/uni_with_join.sql
@@ -41,7 +41,14 @@ ON `departments`.`id` = `degrees`. `department_id`
 ORDER BY `students`.`surname`,`students`.`name` ; 
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-
+SELECT `degrees`.`name` AS `corso di laurea`, `courses`.`name` AS `materia`, `teachers`.`name`, `teachers`.`surname`
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
